0c12c0820e265ce9c2186f7dc350dca1676daee5
Krstan Vjestica <krle@tilda.center>
2020-10-23 12:56:02 +0200

    add clear button

--- bin/box-upgrader.sh
+++ bin/box-upgrader.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box update --box
+clear
 echo "All box upgraded..."
 vagrant box prune
+clear
 echo "... old versions are deleted"
 
