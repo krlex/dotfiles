6f8a21cc2856d0bd6466c15658a2b302da4b044c
Krstan Vjestica <krle@tilda.center>
2021-07-17 13:01:51 +0200

    add for freebsd instructions

--- bin/checktemp.sh
+++ bin/checktemp.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+#
+# For All CPUs on your machine and this is for Freebsd you need to start 
+# `sudo kldload coretemp` 
+# and add  
+# coretemp_load="YES" >> /boot/loader.conf
 
  sudo sysctl -a | grep temperature
